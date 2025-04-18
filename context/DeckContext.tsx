692dca96fa7ba53f48266d537b0cd51d8ee041ae
Axelle-LM <98803198+Axelle-LM@users.noreply.github.com>
2025-04-18 09:42:28 +0200

    assign Card to Deck

--- context/DeckContext.tsx
+++ context/DeckContext.tsx
@@ -3,11 +3,12 @@ import React, { PropsWithChildren, createContext, useContext, useState } from 'r
 export type DeckProps = {
     id: string,
     title: string,
+    cardsId: string[],
 };
 
 const DeckContext = createContext<{
-    id: string, title: string, error: string | null, setId: (id: string) => void, setTitle: (title: string) => void, decks: DeckProps[], addDeck: (title: string) => void
-}>({ id: " ", title: " ", error: null, setId: () => { }, setTitle: () => { }, decks: [], addDeck: () => { } })
+    id: string, title: string, error: string | null, setId: (id: string) => void, setTitle: (title: string) => void, decks: DeckProps[], addDeck: (title: string) => void, cardToDeck: (deckId: string, cardId: string) => void
+}>({ id: " ", title: " ", error: null, setId: () => { }, setTitle: () => { }, decks: [], addDeck: () => { }, cardToDeck: () => { } })
 
 export const DeckProvider = ({ children }: PropsWithChildren) => {
     const [decks, setDecks] = useState<DeckProps[]>([]);
@@ -20,13 +21,24 @@ export const DeckProvider = ({ children }: PropsWithChildren) => {
         const newDeck: DeckProps = {
             id: Date.now().toString(),
             title,
+            cardsId: [],
         };
         setDecks((prev) => [newDeck, ...prev]);
         setTitle('');
     };
 
+    const cardToDeck = (deckId: string, cardId: string) => {
+        setDecks((prev) =>
+            prev.map((deck =>
+                deck.id === deckId ? { ...deck, cardsId: [...new Set([...deck.cardsId, cardId])] }
+                    : deck
+            )
+            )
+        );
+    }
+
     return (
-        <DeckContext.Provider value={{ id, title, error, setId, setTitle, decks, addDeck }}>
+        <DeckContext.Provider value={{ id, title, error, setId, setTitle, decks, addDeck, cardToDeck }}>
             {children}
         </DeckContext.Provider>
     );
