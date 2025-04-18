b6ddc56c89836fedad3bab9af078b8fb50f6e818
Axelle-LM <98803198+Axelle-LM@users.noreply.github.com>
2025-04-19 00:18:57 +0200

    base notification

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, StyleSheet, ScrollView } from "react-native";
 import { FlashcardProvider } from "@/context/FlashcardContext";
 import FlashcardForm from "@/components/FlashcardForm";
@@ -6,8 +6,19 @@ import FlashcardList from "@/components/FlashcardList";
 import { DeckProvider } from "@/context/DeckContext";
 import DeckCreate from "@/components/DeckCreate";
 import DeckList from "@/components/DeckList";
+import { registerForPushNotificationsAsync, scheduleRepeatingNotification } from '../utils/Notification';
 
 export default function Index() {
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) => {
+      if (token) {
+        console.log("Push token:", token);
+        scheduleRepeatingNotification();
+      }
+    });
+  }, []);
+
   return (
     <DeckProvider>
       <FlashcardProvider>
